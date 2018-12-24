0eab8a8f59779e1bc20e814601a18de2dc549f71
Unknown <moonmtwo@gmail.com>
2018-12-24 20:03:48 +0700

    add circular buffer demo

--- circularBufferTest.c
+++ circularBufferTest.c
@@ -1,21 +1,95 @@
+#include <stdio.h>
+
 #include "circularBuffer.h"
+
+#define ESCAPE_CHARACTER (0x1B)
+
+circular_buffer_t Buffer;
+
+int GetUserInput_Interger(void);
+char GetUserInput_Char(void);
+void InitializeBufferFromUserInput(void);
 
 int main(void)
 {
-	circular_buffer_t Buffer;
-	InitializeBuffer(&Buffer, 10);
+	setbuf(stdout, NULL);
+	printf("Circular buffer demo\r\n");
 
-	PrintBufferElements(&Buffer);
-	for(int i = 0; i < 5; i++)
+	char cUserInput = 0;
+	while(cUserInput != 'E' &&   cUserInput != 'e')
 	{
-		Push(i % 10,&Buffer);
-	}
-	for(int i = 0; i < 6; i++)
-	{
-		Pop(&Buffer);
+
+		if(Buffer.piData == 0)
+		{
+			InitializeBufferFromUserInput();
+			continue;
+		}
+
+		printf("Do what: [I]nsert / [P]op / [E]scape \n");
+		cUserInput = GetUserInput_Char();
+		switch(cUserInput)
+		{
+		case 'I':
+		case 'i':
+		{
+			printf("Enter value to insert: \r\n");
+			int iInput = GetUserInput_Interger();
+			Push(iInput, &Buffer);
+		}
+			break;
+		case 'p':
+		case 'P':
+		{
+			Pop(&Buffer);
+		}
+			break;
+		case 'E':
+		case 'e':
+			break;
+		default:
+			printf("You enter [ %c ], not sure what it means\r\n", cUserInput);
+			break;
+		}
 	}
 
-	DestroyBuffer(&Buffer);
+
+	if(Buffer.piData)
+	{
+		DestroyBuffer(&Buffer);
+	}
+	printf("Exiting \r\n");
 	return 0;
 }
 
+
+int GetUserInput_Interger(void)
+{
+	char line[100];         /* a line to be read from the terminal */
+	int answer;             /* an integer chosen by the user */
+	fgets (line, sizeof(line), stdin);      /* read in a line */
+	sscanf (line, "%d", &answer);           /* scan for the integer */
+	return answer;
+}
+
+char GetUserInput_Char(void)
+{
+	char line[100];
+	char answer;
+	fgets(line, sizeof(line), stdin);
+	sscanf(line, "%c", &answer);
+	return answer;
+}
+void InitializeBufferFromUserInput(void)
+{
+	printf("Enter the capacity of the buffer: ");
+	int iCapacity = GetUserInput_Interger();
+	if( iCapacity < 0 )
+	{
+		printf("Invalid parameter! \r\n");
+	}
+	else
+	{
+		InitializeBuffer(&Buffer, iCapacity);
+		printf("Created circular buffer with %d elements\r\n", iCapacity);
+	}
+}
